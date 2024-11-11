8cbeda28ec060e2f77a01cfbecd9fe48ac455e45
Oliver Rietmann <oliver.rietmann@cern.ch>
2024-11-11 19:37:06 +0100

    Add examples for all wrapper types

--- main.cpp
+++ main.cpp
@@ -16,10 +16,12 @@ struct S {
 
     int abs2() const { return x * x + y * y; }
     int& getX() { return x; }
+    const int& getX() const { return x; }
     void setX(int x_new) { x = x_new; }
 };
 
 using wrapper_type = wrapper::wrapper<std::vector, S, wrapper::layout::soa>;
+
 S<wrapper::identity> eval_const_at(const wrapper_type& w, std::size_t i) { return w[i]; }
 
 int main() {
@@ -27,16 +29,16 @@ int main() {
 
     // AoS access
     for (int i = 0; i < 3; ++i) {
-        my_array[i].x = i - 10;
-        my_array[i].y = i + 50;
-        my_array[i].activation = {0.5 * i, 0.5 * i};
-        my_array[i].identifier = "foo" + std::to_string(i);
+        S<wrapper::reference> r = my_array[i];
+        r.setX(i - 10);
+        r.y = i + 50;
+        r.activation = {0.5 * i, 0.5 * i};
+        r.identifier = "foo" + std::to_string(i);
     }
 
     // const_reference
     for (int i = 0; i < 3; ++i) {
-        S<wrapper::identity> r = my_array[i];
-        r.setX(42);
+        S<wrapper::const_reference> r = my_array[i];
         std::cout << "Element " << i << ": {"
                 << r.x << ", " << r.y << ", {"
                 << r.activation.x << ", " << r.activation.y << "}, "
@@ -45,9 +47,9 @@ int main() {
 
     // member functions
     for (int i = 0; i < 3; ++i) {
-        S<wrapper::identity> r = eval_const_at(my_array, i);
-        std::cout << "my_array[" << i << "].getX()" << " == " << r.getX() << ", ";
-        std::cout << "my_array[" << i << "].abs2() == "<< r.abs2() << std::endl;
+        S<wrapper::identity> cr = eval_const_at(my_array, i);
+        std::cout << "my_array[" << i << "].getX()" << " == " << cr.getX() << ", ";
+        std::cout << "my_array[" << i << "].abs2() == "<< cr.abs2() << std::endl;
     }
 
     return 0;
