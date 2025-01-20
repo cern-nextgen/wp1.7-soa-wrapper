1c40d7e6217a4d6b1376f357f5bb838f47187d56
Oliver Rietmann <oliver.rietmann@cern.ch>
2025-01-20 11:15:47 +0100

    Move default_wrapper to factory

--- factory.h
+++ factory.h
@@ -60,6 +60,34 @@ wrapper::wrapper<pmr::vector, S, L> make_wrapper(char* buffer, std::size_t bytes
     else if constexpr (L == wrapper::layout::soa) return make_wrapper_soa<S>(buffer, bytes);
 }
 
+template <
+    template <class> class F,
+    template <template <class> class> class S
+>
+wrapper::wrapper<F, S, wrapper::layout::aos> default_aos_wrapper(std::size_t N) {
+    return { F<S<wrapper::value>>(N) };
+}
+
+template <
+    template <class> class F,
+    template <template <class> class> class S
+>
+wrapper::wrapper<F, S, wrapper::layout::soa> default_soa_wrapper(std::size_t N) {
+    constexpr static std::size_t M = helper::CountMembers<S<wrapper::value>>();
+    auto forward_to_F_constructor = [N](auto member, std::size_t) -> decltype(auto) { return F<decltype(member)>(N); };
+    return { helper::apply_to_members<M, S<wrapper::value>, S<F>>(S<wrapper::value>(), forward_to_F_constructor)  };
+}
+
+template <
+    template <class> class F,
+    template <template <class> class> class S,
+    wrapper::layout L
+>
+wrapper::wrapper<F, S, L> default_wrapper(std::size_t N) {
+    if constexpr (L == wrapper::layout::aos) return default_aos_wrapper<F, S>(N);
+    else if constexpr (L == wrapper::layout::soa) return default_soa_wrapper<F, S>(N);
+}
+
 }  // namespace factory
 
 #endif  // FACTORY_H
--- unittests.cpp
+++ unittests.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <span>
 #include <string>
-#include <vector>
 
 #include <gtest/gtest.h>
 
@@ -26,38 +25,12 @@ struct S {
     void setX(auto x_new) { x = x_new; }
 };
 
-template <
-    template <class> class F,
-    template <template <class> class> class S
->
-wrapper::wrapper<F, S, wrapper::layout::aos> default_aos_wrapper(std::size_t N) {
-    return { F<S<wrapper::value>>(N) };
-}
-
-template <
-    template <class> class F,
-    template <template <class> class> class S
->
-wrapper::wrapper<F, S, wrapper::layout::soa> default_soa_wrapper(std::size_t N) {
-    return { S<F>{ F<int>(N), F<int>(N), F<Point2D>(N), F<std::string>(N) } };
-}
-
-template <
-    template <class> class F,
-    template <template <class> class> class S,
-    wrapper::layout L
->
-wrapper::wrapper<F, S, L> default_wrapper(std::size_t N) {
-    if constexpr (L == wrapper::layout::aos) return default_aos_wrapper<F, S>(N);
-    else if constexpr (L == wrapper::layout::soa) return default_soa_wrapper<F, S>(N);
-}
-
 template <class L, class R>
 bool operator==(L l, R r) { return l.x == r.x && l.y == r.y && l.point == r.point && l.identifier == r.identifier; }
 bool operator==(Point2D l, Point2D r) { return l.x == r.x && l.y == r.y; }
 
 TEST(UnitTests, ProxyType) {
-    auto dw = default_wrapper<debug::vector, S, wrapper::layout::soa>(1);
+    auto dw = factory::default_wrapper<debug::vector, S, wrapper::layout::soa>(1);  // aos
     S<wrapper::value> s = {1, 2, {3.0, 4.0}, "Test"};
     dw[0].x = s.x; dw[0].y = s.y; dw[0].point = s.point; dw[0].identifier = s.identifier;
 
