a8cd6b35a6be5cf5a308399af925fb4f334c906e
Oliver Gregor Rietmann <orietman@orietman.dyndns.cern.ch>
2025-02-05 11:54:53 +0100

    cleanup

--- kernel.cu
+++ kernel.cu
@@ -1,7 +1,7 @@
 #include "kernel.h"
 
 #include <cmath>
-#include <iostream>
+#include <iostream>  // This include fixes segfault due to uninitialized std::cout, even if the latter is not used
 
 #include "gpu.h"
 #include "skeleton.h"
@@ -9,6 +9,10 @@
 
 
 namespace kernel {
+
+void print_cuda_error(cudaError_t err) {
+    if (err != cudaSuccess) std::cout << cudaGetErrorString(err) << std::endl;
+}
 
 template <class T>
 struct raw_array {
@@ -24,19 +28,10 @@ void add(int N, wrapper::wrapper<raw_array, S, wrapper::layout::aos> w) {
     for (int i = index; i < N; i += stride) w[i].y = w[i].x + w[i].y;
 }
 
-float run() {
+int run() {
     int N = 8;
     wrapper::wrapper<raw_array, S, wrapper::layout::aos> w;
-
-    cudaError_t err = cudaMallocManaged(&w.data.data, N * sizeof(S<wrapper::value>));
-    if (err != cudaSuccess) {
-        std::cerr << "CUDA malloc failed: " << cudaGetErrorString(err) << std::endl;
-    } else {
-        std::cout << cudaSuccess << ", " << err << std::endl;
-    }
-
-
-    // Initialize on the host
+    print_cuda_error(cudaMallocManaged(&w.data.data, N * sizeof(S<wrapper::value>)));
     for (int i = 0; i < N; ++i) {
         S<wrapper::reference> r = w[i];
         r.setX(1);
@@ -45,32 +40,14 @@ float run() {
         r.identifier = 0.1 * i;
     }
 
-    // Run kernel
     // int blockSize = 1;
     // int numBlocks = (N + blockSize - 1) / blockSize;
     add<<<1, 1>>>(N, w);
-
-    err = cudaDeviceSynchronize();
-    if (err != cudaSuccess) {
-        std::cerr << "CUDA cudaDeviceSynchronize failed: " << cudaGetErrorString(err) << std::endl;
-    } else {
-        std::cout << cudaSuccess << ", " << err << std::endl;
-    }
+    print_cuda_error(cudaDeviceSynchronize());
 
     int maxError = 0;
-    for (int i = 0; i < N; ++i) {
-        //std::cout << "(" << w[i].x << ", " << w[i].y << "), ";
-        maxError = std::max(maxError, std::abs(w[i].y - 3));
-    }
-
-    err = cudaFree(w.data.data);
-    if (err != cudaSuccess) {
-        std::cerr << "CUDA cudaFree failed: " << cudaGetErrorString(err) << std::endl;
-    } else {
-        std::cout << cudaSuccess << ", " << err << std::endl;
-    }
-    w.data.data = nullptr;
-
+    for (int i = 0; i < N; ++i) maxError = std::max(maxError, std::abs(w[i].y - 3));
+    print_cuda_error(cudaFree(w.data.data));
     return maxError;
 }
 
--- main.cpp
+++ main.cpp
@@ -2,19 +2,14 @@
 // #include <span>
 // #include <vector>
 
-#include "allocator.h"
-#include "debug.h"
-#include "factory.h"
 #include "kernel.h"
-#include "skeleton.h"
-#include "wrapper.h"
 
 // template <class T>
 // using my_vector = std::vector<T>;  // avoid clang bug about default template parameters
 
 int main() {
 
-    float error = kernel::run();
+    int error = kernel::run();
     std::cout << "Kernel output: " << error << std::endl;
 
     return 0;
